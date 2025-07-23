d697285a954e01820ca6804be024590ba1d6f3be
Mempatch <222099868+mempatcher@users.noreply.github.com>
2025-07-23 08:19:10 +0200

    Added main icon and created a NotifyIcon object

--- TaskbarShortcuts/TrayForm.cs
+++ TaskbarShortcuts/TrayForm.cs
@@ -2,9 +2,25 @@ namespace TaskbarShortcuts
 {
     public partial class TrayForm : Form
     {
+        private NotifyIcon trayIcon;
+
         public TrayForm()
         {
-            InitializeComponent();
+            //InitializeComponent();
+
+            trayIcon = new NotifyIcon()
+            {
+                Text = "Taskbar shortcuts",
+                Icon = Properties.Resources.ApplicationIcon,
+                Visible = true
+            };
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+            base.OnLoad(e);
         }
     }
 }
